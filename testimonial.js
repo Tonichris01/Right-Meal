50b44f9666dcd2eab4132ae7a69a1e2d3a99ff0a
Tonichris01 <gemsbatinterns@gmail.com>
2023-07-28 02:03:50 +0100

    saving testimonials to localstorage

--- testimonial.js
+++ testimonial.js
@@ -14,62 +14,28 @@ function closeTestimonialCardbox() {
 }
 
 // Function to add testimonial
-function addTestimonial() {
-    const testimonialText = testimonialForm.elements['testimonial'].value;
-    const testimonialAuthor = testimonialForm.elements['name'].value;
-
-    if (testimonialText && testimonialAuthor) {
-        const testimonial = document.createElement('div');
-        testimonial.classList.add('testimonial');
-        testimonial.innerHTML = `
-      <p class="testimonial-text">"${testimonialText}"</p>
-      <h4 class="testimonial-author">${testimonialAuthor}</h4>
-      <button class="delete-button" onclick="deleteTestimonial(this)">&#10006;</button>
-    `;
-        testimonialSlider.appendChild(testimonial);
-        closeTestimonialCardbox();
-    }
-}
-
-// Function to add testimonial
 // function addTestimonial() {
 //     const testimonialText = testimonialForm.elements['testimonial'].value;
 //     const testimonialAuthor = testimonialForm.elements['name'].value;
 
 //     if (testimonialText && testimonialAuthor) {
-//         const xhr = new XMLHttpRequest();
-//         xhr.open("POST", "store_testimonial.php", true);
-//         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//         xhr.onreadystatechange = function() {
-//             if (xhr.readyState === XMLHttpRequest.DONE) {
-//                 if (xhr.status === 200) {
-//                     // Testimonial added successfully
-//                     const testimonial = document.createElement('div');
-//                     testimonial.classList.add('testimonial');
-//                     testimonial.innerHTML = `
-//                         <p class="testimonial-text">"${testimonialText}"</p>
-//                         <h4 class="testimonial-author">${testimonialAuthor}</h4>
-//                         <button class="delete-button" onclick="deleteTestimonial(this)">&#10006;</button>
-//                     `;
-//                     testimonialSlider.appendChild(testimonial);
-//                     closeTestimonialCardbox();
-//                 } else {
-//                     // Handle error here
-//                     console.error("Failed to add testimonial.");
-//                 }
-//             }
-//         };
-//         const data = `testimonial=${encodeURIComponent(testimonialText)}&name=${encodeURIComponent(testimonialAuthor)}`;
-//         xhr.send(data);
+//         const testimonial = document.createElement('div');
+//         testimonial.classList.add('testimonial');
+//         testimonial.innerHTML = `
+//       <p class="testimonial-text">"${testimonialText}"</p>
+//       <h4 class="testimonial-author">${testimonialAuthor}</h4>
+//       <button class="delete-button" onclick="deleteTestimonial(this)">&#10006;</button>
+//     `;
+//         testimonialSlider.appendChild(testimonial);
+//         closeTestimonialCardbox();
 //     }
 // }
 
-
 // Function to delete testimonial
-function deleteTestimonial(button) {
-    const testimonial = button.parentNode;
-    testimonialSlider.removeChild(testimonial);
-}
+// function deleteTestimonial(button) {
+//     const testimonial = button.parentNode;
+//     testimonialSlider.removeChild(testimonial);
+// }
 
 // Form submission event
 // testimonialForm.addEventListener('submit', function(e) {
@@ -83,6 +49,78 @@ testimonialCardbox.addEventListener('click', function(e) {
         closeTestimonialCardbox();
     }
 });
+
+  //using localstorage
+   // Function to add testimonial
+   function addTestimonial(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const testimonialText = testimonialForm.elements['testimonial'].value;
+    const testimonialAuthor = testimonialForm.elements['name'].value;
+
+    if (testimonialText && testimonialAuthor) {
+        const testimonial = {
+            text: testimonialText,
+            author: testimonialAuthor,
+        };
+
+        // Check if testimonials already exist in localStorage
+        const testimonials = JSON.parse(localStorage.getItem('testimonials')) || [];
+        testimonials.push(testimonial);
+
+        // Save updated testimonials to localStorage
+        localStorage.setItem('testimonials', JSON.stringify(testimonials));
+
+        // Add testimonial to the page
+        const testimonialDiv = document.createElement('div');
+        testimonialDiv.classList.add('testimonial');
+        testimonialDiv.innerHTML = `
+            <p class="testimonial-text">"${testimonialText}"</p>
+            <h4 class="testimonial-author">${testimonialAuthor}</h4>
+            <button class="delete-button" onclick="deleteTestimonial(this)">&#10006;</button>
+        `;
+        testimonialSlider.appendChild(testimonialDiv);
+        closeTestimonialCardbox();
+    }
+}
+
+// Function to retrieve testimonials from localStorage and display them on the page
+function displayTestimonials() {
+    const testimonials = JSON.parse(localStorage.getItem('testimonials')) || [];
+
+    testimonials.forEach((testimonial) => {
+        const testimonialDiv = document.createElement('div');
+        testimonialDiv.classList.add('testimonial');
+        testimonialDiv.innerHTML = `
+            <p class="testimonial-text">"${testimonial.text}"</p>
+            <h4 class="testimonial-author">${testimonial.author}</h4>
+            <button class="delete-button" onclick="deleteTestimonial(this)">&#10006;</button>
+        `;
+        testimonialSlider.appendChild(testimonialDiv);
+    });
+}
+
+// Function to delete testimonial
+function deleteTestimonial(button) {
+    const testimonialDiv = button.parentNode;
+    const testimonials = JSON.parse(localStorage.getItem('testimonials')) || [];
+
+    // Remove the deleted testimonial from the array
+    const index = Array.from(testimonialSlider.children).indexOf(testimonialDiv);
+    if (index !== -1) {
+        testimonials.splice(index, 1);
+        localStorage.setItem('testimonials', JSON.stringify(testimonials));
+    }
+
+    // Remove testimonial from the page
+    testimonialSlider.removeChild(testimonialDiv);
+}
+
+// Form submission event
+testimonialForm.addEventListener('submit', addTestimonial);
+
+// Load existing testimonials from localStorage when the page loads
+displayTestimonials();
 
 // sign outbutton
 const signOutIcon = document.getElementById("signOutIcon");
